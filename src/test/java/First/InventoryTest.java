17e5d548c9ebf2ccd14f2a4bbabec45685c0397b
Michał Roziel <rozielmichal@gmail.com>
2024-06-25 17:03:38 +0200

    added test class

---
+++ src/test/java/First/InventoryTest.java
@@ -0,0 +1,49 @@
+package First;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryTest {
+
+    @org.junit.jupiter.api.Test
+    void addProduct() {
+
+
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeProduct() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void searchProductById() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findProductsByCategory() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllProducts() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sortProductsByName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sortProductsByPrice() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getLowStockProducts() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void filterProducts() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void applyToProducts() {
+    }
+}
