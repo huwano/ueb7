c3f433091018cb727150ca539d99e468b12845f4
huwano <akirpap03@gmail.com>
2024-06-25 13:54:51 +0200

    adding consume

--- src/main/java/Second/Consumer.java
+++ src/main/java/Second/Consumer.java
@@ -1,4 +1,12 @@
 package Second;
 
 public class Consumer {
+    public int consume(int number) {
+        int sum = 0;
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
 }
